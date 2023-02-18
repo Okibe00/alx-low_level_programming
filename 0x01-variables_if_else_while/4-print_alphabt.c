3b070504ba414219c0b2d5c4b31060eb48a4747f
Okibe Onmeje <okibeogomola@gmail.com>
2023-02-18 03:52:08 -0800

    4-print_alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+* main - entry point
+* description: print alphabets using putchar
+* Return: 0 (success)
+*/
+int main(void)
+{
+	char l[] = "abcdefghijklmnopqrstuvwxyz";
+	int n = 0;
+
+	while (n < 26)
+	{
+		if (l[n] == 'q' || l[n] == 'e')
+		{
+			n++;
+		}
+		putchar(l[n]);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
+
