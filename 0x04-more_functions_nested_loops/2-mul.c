72a598dc44f2746fe2a430c93eaab5e22e0ff150
Okibe Onmeje <okibeogomola@gmail.com>
2023-02-23 11:56:33 -0800

    2-mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+/**
+ * mul - multiply two ints
+ * @a: first int
+ * @b: second int
+ * description: multiply two ints
+ *
+ * Return: Always 0.
+ */
+
+int mul(int a, int b)
+{
+	int res = (a * b);
+
+	return (res);
+}
