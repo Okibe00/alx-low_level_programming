83903a239bcead452a89874f73ff416c1eb9c2fb
Okibe Onmeje <okibeogomola@gmail.com>
2023-03-09 06:28:53 -0800

    0-puts_recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+* _puts_recursion - prints a string
+* @s: string to print
+* description: prints a string s
+*/
+
+
+
+
+void _puts_recursion(char *s)
+{
+	_putchar(*(s));
+	++s;
+
+	if (*s != '\0')
+	{
+		_puts_recursion(s);
+	}
+	if (*s == '\0')
+	{
+		_putchar('\n');
+	}
+}
+
