f285ffa1d81619e0c2d28723514e05b59196d9b7
Okibe Onmeje <okibeogomola@gmail.com>
2023-03-09 19:03:38 -0800

    3-factorial

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,24 @@
+/**
+* factorial - print fact
+* @n: int
+* description: print factorial of a num
+* Return: int
+*/
+
+
+
+int factorial(int n)
+{
+	if (n == 0)
+	{
+		return (1);
+	}
+	else if (n < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (n * factorial(n - 1));
+	}
+}
