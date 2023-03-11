eb0087b25e94d8872bee5086013e691b10b45259
Okibe Onmeje <okibeogomola@gmail.com>
2023-03-11 05:43:46 -0800

    0-whatsmyname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* main - entry point
+* @argc: int
+* @argv: string
+* decription: prints program name
+* Return: 0
+*/
+int main(int __attribute__((unused))argc, char __attribute__((unused)) *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
