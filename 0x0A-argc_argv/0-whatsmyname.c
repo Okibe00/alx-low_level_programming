0c467cbe83dca4380054d0366b409991ff95f13e
okibe onmeje <okibeogomola@gmail.com>
2023-07-07 11:27:35 -0700

    add support to print program name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+  * main - printd progam name
+  * @argc: argument count
+  * @argv: argument vector
+  * description: prints the name of the program
+  * Return: 0 success
+*/
+
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
+
+
