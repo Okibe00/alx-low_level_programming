81428b9e67d72ecd2f8a28dadc30ff1944a0c003
okibe onmeje <okibeogomola@gmail.com>
2023-07-07 12:47:16 -0700

    add 1-args.c
    prints argc minus 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,19 @@
+
+#include "main.h"
+/**
+  * main - printd progam name
+  * @argc: argument count
+  * @argv: argument vector
+  * description: prints the name of the program
+  * Return: 0 success
+*/
+
+
+int main(int argc, char *argv[])
+{
+	(void) argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
+
+
