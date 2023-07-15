5d78cafe4e54f4b91ff2d4995d439f7dcc79d6f8
okibe onmeje <okibeogomola@gmail.com>
2023-07-14 21:40:36 -0700

    addd 100-realloc
    reallocates a block of memory

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -9,4 +9,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 unsigned int _strlen_recursion(char *s);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+void *_memcpy(void *dest, void *src, size_t n);
 #endif
