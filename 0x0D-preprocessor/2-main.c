ad35ce2acff094b4e5eb88db225704ef6ac80b6f
okibe onmeje <okibeogomola@gmail.com>
2023-07-17 04:52:41 -0700

    add 2-main.c
    prints program name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+  * main - prints program name
+  * description: prints program name ussing file macro
+  * Return: 0
+*/
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
