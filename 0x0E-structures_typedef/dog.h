4721033a706b68047a05c6b321c5914c48a594ae
okibe onmeje <okibeogomola@gmail.com>
2023-07-17 13:42:36 -0700

    add .gitignore README.md dog.h
    create a dog struct template

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+  * struct dog - dog information
+  * @name: name of dog
+  * @age: age of dog
+  * @owner: dog owner
+  * description: holds dog information
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
