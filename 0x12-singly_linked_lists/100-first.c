b93c678958c11acd4801e5ec2b43d99ca145ec22
okibe onmeje <okibeogomola@gmail.com>
2023-07-28 09:37:58 -0700

    add function to print before main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+  * print - prints a string before main
+  * description: prints a string before main
+  * Return: Nothing
+*/
+
+void print(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -24,4 +24,5 @@ size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 int _strlen_recursion(char *s);
 void free_list(list_t *head);
+void print(void)__attribute__((constructor));
 #endif
