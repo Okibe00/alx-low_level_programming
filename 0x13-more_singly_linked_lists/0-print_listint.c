f3cdd46ac99c7d913d531176af4eff254d0f8566
okibe onmeje <okibeogomola@gmail.com>
2023-08-01 04:22:05 -0700

    add print number of node

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+  * print_listint - prints elements of a linkd list
+  * @h: head node
+  * description: prints the elements of a linked list
+  * Return: number
+*/
+
+
+
+size_t print_listint(const listint_t *h)
+{
+	const listint_t *temp;
+	size_t num;
+
+	if (h == NULL)
+		return (-1);
+	temp = h;
+	num = 0;
+	while (temp != NULL)
+	{
+		printf("%d\n", temp->n);
+		num++;
+		temp = temp->next;
+	}
+	return (num);
+}
