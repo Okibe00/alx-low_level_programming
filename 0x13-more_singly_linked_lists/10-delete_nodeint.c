ed8851942c475d48b2d5fc5bdf503ddb7c8d54e7
okibe onmeje <okibeogomola@gmail.com>
2023-08-02 05:28:53 -0700

    fix corrected return value from 0 to 1

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -22,7 +22,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	{
 		*head = temp->next;
 		free(temp);
-		return (0);
+		return (1);
 	}
 	for (i = 0; i < index - 1 && temp != NULL; i++)
 		temp = temp->next;
@@ -33,7 +33,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 			return (-1);
 		temp->next = temp1->next;
 		free(temp1);
-		return (0);
+		return (1);
 	}
 	else
 	{
