8f91ad56d9cb44fb40e0cef32f5a4a7e9af85460
okibe onmeje <okibeogomola@gmail.com>
2023-08-09 06:38:59 -0700

    fix check write return

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -56,8 +56,8 @@ void file_cpy(char *src, char *dest)
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", src);
 		exit(98);
 	}
-	bytes_w = write(fd_dest, buff, bytes_r);
-	if (bytes_w == -1)
+	bytes_w = write(fd_dest, buff, bytes_r); 
+	if (bytes_w < 0)
 	{
 		_close(fd_src);
 		_close(fd_dest);
